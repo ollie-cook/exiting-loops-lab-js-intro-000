2f795a8c9df6d923edf462c1ed75b07a1d05bd38
Oliver Cook <olliewatford@gmail.com>
2020-06-24 21:21:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function keepGoing(array, changeValue, skipValue){
 
 function findBy(array,findFn){
   for (let i=0;i<array.length;i++){
-    if(array[i]===findFn){
+    if(array[i]===c){
       return array[i]
-    }
+    } 
   }
   return null
 }
